9cdbae8076dfa5e8dbf73f3ed1dfc304aa35743f
Abysman <756884429@qq.com>
2018-08-08 19:47:29 +0800

    88.Merge Sorted Array_v1.0

--- 88.Merge Sorted Array/88.Merge Sorted Array/main.cpp
+++ 88.Merge Sorted Array/88.Merge Sorted Array/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  88.Merge Sorted Array
 //
-//  Created by Abysman on 2018/8/2.
+//  Created by Abysman on 2018/8/8.
 //  Copyright © 2018年 Abysman. All rights reserved.
 //
 
@@ -11,48 +11,60 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//        int i = m - 1, j = n - 1, tar = m + n - 1;
+//        while (j >= 0) {
+//            nums1[tar--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+//        }
+//    }
+//};
+
+
+//  mine
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0; i < m; ++i) {
-            cout << "nums1:" << nums1[i] << "\n";
-            if (nums1[i] <= nums2[0]) {
-                continue;
+        int i = m - 1;
+        int j = n - 1;
+        int count = 0;
+        while (count < n) {
+            if (nums1[i] < nums2[j]) {
+                nums1[nums1.size() - 1 - count] = nums2[j];
+                j--;
             }
-            if (nums1[i] >= nums2[nums2.size() - 1]) {
-                swap(nums1[i], nums2[nums2.size() - 1]);
-                continue;
+            else {
+                nums1[nums1.size() - 1 - count] = nums1[i];
+                i--;
             }
-            for (int j = 0; j < nums2.size(); ++j) {
-                cout << "nums2:" << nums2[j] << "\n";
-                if (nums1[i] >= nums2[j]) {
-                    swap(nums1[i], nums2[j]);
-                    i--;
-                    break;
-                }
+            count += 1;
+        }
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] < nums2[j]) {
+                nums1[i + j + 1] = nums2[j];
+                j--;
+            }
+            else {
+                nums1[i + j + 1] = nums1[i];
+                i--;
             }
         }
-        for (int k = 0; k < nums2.size(); ++k) {
-            nums1[m + k] = nums2[k];
+        while (j >= 0) {
+            nums1[j] = nums2[j];
+            j--;
         }
-    }
-    
-    void swap(int& a, int& b) {
-        int tmp = a;
-        a = b;
-        b = tmp;
     }
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    vector<int> a = {1,2,3,0,0,0};
-    vector<int> b = {2, 5, 6};
     Solution sol = Solution();
-    sol.merge(a, 3, b, b.size());
-    for (auto t:a) {
-        cout << t << "\n";
+    vector<int> a = {2,0};
+    vector<int> b = {1};
+    sol.merge(a, 1, b, 1);
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i] << "\n";
     }
-    std::cout << "Hello, World!\n";
     return 0;
 }
