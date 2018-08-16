7f2ded928095e0b6f846724e58baa7d8d01f4448
Abysman <756884429@qq.com>
2018-08-16 10:57:53 +0800

    28.Implement strStr()_v1.0

--- 28.Implement strStr()/28.Implement strStr()/main.cpp
+++ 28.Implement strStr()/28.Implement strStr()/main.cpp
@@ -2,47 +2,61 @@
 //  main.cpp
 //  28.Implement strStr()
 //
-//  Created by Abysman on 2018/8/8.
+//  Created by Abysman on 2018/8/16.
 //  Copyright © 2018年 Abysman. All rights reserved.
 //
 
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
+    vector<int> calc_next(string s) {
+        int i = 0, j = -1;
+        vector<int> next(s.length() + 1, 0);
+        next[0] = -1;
+        while (i < s.length()) {
+            if (j == -1 || s[i] == s[j]) {
+                i++;
+                j++;
+                next[i] = j;
+            }
+            else {
+                j = next[j];
+            }
+        }
+        return next;
+    }
+    
     int strStr(string haystack, string needle) {
-        int index = 0;
-        if (needle.empty()) return index;
-        if (haystack.length() < needle.length()) return -1;
+        vector<int> next = calc_next(needle);
         int i = 0, j = 0;
-        while (i <= haystack.length() - needle.length()) {
-            index = i;
-            while (haystack[i] == needle[j] && j < needle.length()) {
+        int h_length = haystack.length();
+        int n_length = needle.length();
+        while (i < h_length && j < n_length) {
+            if (j == -1 || haystack[i] == needle[j]) {
                 i++;
                 j++;
             }
-            if (j != needle.length()) {
-                j = 0;
-                index = -1;
-                i++;
-            }
             else {
-                break;
+                j = next[j];
             }
+            
         }
-        return index;
+        if (j == needle.length()) {
+            return i - j;
+        }
+        return -1;
     }
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    string haystack = "mississippi";
+    string needle = "issipi";
     Solution sol = Solution();
-    string a = "mississippi";
-    string b = "issip";
-    cout << sol.strStr(a, b);
-    std::cout << "Hello, World!\n";
+    cout << sol.strStr(haystack, needle) << endl;
     return 0;
 }
