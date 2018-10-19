d386920cb88ea121d5a989a937d631ee6e87a92f
Abysman <756884429@qq.com>
2018-10-18 21:11:57 -0700

    54.Spiral Matrix_v1.0

---
+++ 54.Spiral Matrix/54.Spiral Matrix/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  54.Spiral Matrix
+//
+//  Created by Abysman on 2018/10/18.
+//  Copyright © 2018年 Abysman. All rights reserved.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return {};
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> spiral(m * n);
+        int u = 0, d = m - 1, l = 0, r = n - 1, k = 0;
+        while (true) {
+            // up
+            for (int col = l; col <= r; col++) spiral[k++] = matrix[u][col];
+            if (++u > d) break;
+            // right
+            for (int row = u; row <= d; row++) spiral[k++] = matrix[row][r];
+            if (--r < l) break;
+            // down
+            for (int col = r; col >= l; col--) spiral[k++] = matrix[d][col];
+            if (--d < u) break;
+            // left
+            for (int row = d; row >= u; row--) spiral[k++] = matrix[row][l];
+            if (++l > r) break;
+        }
+        return spiral;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
