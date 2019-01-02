8ce13bf585f74bcba6848a9f80ff00de4079bd03
Abysman <756884429@qq.com>
2019-01-02 09:33:32 -0800

    292.Nim Game_v1.0

---
+++ 292.Nim Game/292.Nim Game/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  292.Nim Game
+//
+//  Created by Abysman on 2019/1/2.
+//  Copyright © 2019 Abysman. All rights reserved.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n%4==0;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
