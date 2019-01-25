043d70a70e63706725189736102944ac9b614b31
Abysman <756884429@qq.com>
2019-01-24 23:39:27 -0800

    5.Longest Palindromic Substring_v1.1

--- 5.Longest Palindromic Substring/5.Longest Palindromic Substring/main.cpp
+++ 5.Longest Palindromic Substring/5.Longest Palindromic Substring/main.cpp
@@ -14,44 +14,37 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int len = s.length();
-        int maxLen = 0;
-        int res = -1;
         if (len < 2) return s;
-        for (int i = 0; i <= len - 1; ++i) {
+        int i = 0, j = len - 1, maxLen = 0, index = 0, currLen = 0;
+        for (int i = 0; i < len; ++i) {
             if (len - i <= maxLen / 2) break;
-            if (s[i] == s[i + 1]) {
-                int currLen = helper(s, i, 1);
+            if (s[i] == s[i+1]) {
+                currLen = helper(s, i, 1);
                 if (currLen > maxLen) {
                     maxLen = currLen;
-                    res = i;
+                    index = i;
                 }
             }
-            int currLen = helper(s, i, 0);
+            currLen = helper(s, i, 0);
             if (currLen > maxLen) {
                 maxLen = currLen;
-                res = i;
+                index = i;
             }
         }
-        return s.substr(res - (maxLen - 1) / 2, maxLen);
+        return s.substr(index - (maxLen - 1) / 2, maxLen);
     }
     
-    int helper(string s, int index,int flag) {
-        int len = s.length(), i , j;
-        i = index - 1;
-        j = index + 1;
+    int helper(string s, int index, int flag) {
+        int i = index - 1;
+        int j = index + 1;
         int currLen = 1;
         if (flag == 1) {
             i = index;
             currLen = 0;
         }
-        while (i >= 0 && j <= len - 1) {
-            if (s[i] == s[j]) {
-                currLen += 2;
-                i--;
-                j++;
-                continue;
-            }
-            return currLen;
+        while (i >= 0 && j <= s.length() - 1) {
+            if (s[i--] == s[j++]) currLen += 2;
+            else return currLen;
         }
         return currLen;
     }
