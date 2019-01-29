19f009de505c4e6e8c2575dbaceab177dcd7bcca
Abysman <756884429@qq.com>
2019-01-29 15:46:28 -0800

    004.High Five(Amazon)_v1.0

--- 004.High Five(Amazon)/004.High Five(Amazon)/main.cpp
+++ 004.High Five(Amazon)/004.High Five(Amazon)/main.cpp
@@ -7,6 +7,41 @@
 //
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+
+using namespace std;;
+
+struct Result {
+    int id, score;
+    Result (int id , int score) {
+        id = id;
+        score = score;
+    }
+};
+
+map<int, double> getHighFive(vector<Result> scores) {
+    map<int, priority_queue<int, vector<int>>> pValue;
+    for (Result res: scores) {
+        int id = res.id;
+        if (pValue.find(id) != pValue.end()) {
+            pValue[id].push(res.score);
+            if (pValue[id].size() > 5) pValue[id].pop();
+        }
+    }
+    map<int, double> res;
+    for (auto &p:pValue) {
+        double sum = 0;
+        priority_queue<int, vector<int>> curr = p.second;
+        for (int i = 0; i < 5; ++i) {
+            sum += curr.top();
+            curr.pop();
+        }
+        res[p.first] = sum / 5.0;
+    }
+    return res;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
