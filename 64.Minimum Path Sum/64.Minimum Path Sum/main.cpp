5889a6d3c755f93a451f63f54f3e9ad0a268eaa5
Abysman <756884429@qq.com>
2019-01-29 09:52:50 -0800

    64.Minimum Path Sum_v1.1

--- 64.Minimum Path Sum/64.Minimum Path Sum/main.cpp
+++ 64.Minimum Path Sum/64.Minimum Path Sum/main.cpp
@@ -32,20 +32,19 @@ using namespace std;
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        if (grid.empty()) return 0;
-        vector<vector<int>> res(grid.size(), vector<int> (grid[0].size(), 0));
-        res[0][0] = grid[0][0];
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[0].size(); ++j) {
-                if (i == 0 && j == 0) continue;
-                if (i == 0) res[i][j] = res[i][j - 1] + grid[i][j];
-                else if (j == 0) res[i][j] = res[i - 1][j] + grid[i][j];
-                else {
-                    res[i][j] = min(res[i - 1][j], res[i][j - 1]) + grid[i][j];
-                }
+        if (grid.empty() || grid[0].empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+        vector<int> tmp(n);
+        vector<int> res(m, INT_MAX);
+        res[0] = grid[0][0];
+        for (int i = 1; i < m; ++i) res[i] = grid[i][0] + res[i - 1];
+        for (int col = 1; col < n; ++col) {
+            for (int row = 0; row < m; ++row) {
+                if (row >= 1) res[row] = min(res[row - 1], res[row]);
+                res[row] += grid[row][col];
             }
         }
-        return res[grid.size() - 1][grid[0].size() - 1];
+        return res[m - 1];
     }
 };
 
