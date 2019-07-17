0a54e51c3692161fa42a85bb8f33356a83b6a6d5
steven xu <stevenxu169@hotmail.com>
2019-07-16 23:59:10 -0700

    18.4Sum

---
+++ 18.4Sum/18.4Sum/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  18.4Sum
+//
+//  Created by stevenxu on 7/16/19.
+//  Copyright © 2019 stevenxu. All rights reserved.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        if (n < 4) return res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if (nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) continue;
+            for (int j = i + 1; j < n - 2; ++j) {
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if (nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target) continue;
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    int currSum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (currSum < target) {
+                        left++;
+                    } else if (currSum > target) {
+                        right--;
+                    } else {
+                        res.push_back(vector<int>{nums[i], nums[j], nums[left], nums[right]});
+                        do {left++;} while (nums[left] == nums[left - 1] && left < right);
+                        do {right--;} while (nums[right] == nums[right + 1] && left < right);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
