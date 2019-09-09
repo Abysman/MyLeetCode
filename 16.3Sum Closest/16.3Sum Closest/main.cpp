665071b75d607485faed31f0282e9cfd6fb299aa
steven xu <stevenxu169@hotmail.com>
2019-09-08 20:42:36 -0700

    16.3Sum Closest

--- 16.3Sum Closest/16.3Sum Closest/main.cpp
+++ 16.3Sum Closest/16.3Sum Closest/main.cpp
@@ -8,6 +8,29 @@
 
 #include <iostream>
 
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int num_size = nums.size();
+        if (num_size < 3) return 0;
+        sort(nums.begin(), nums.end());
+        int res = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < num_size - 2; ++i) {
+            int left = i + 1, right = num_size - 1;
+            
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                int curr_sum = nums[i] + nums[left] + nums[right];
+                if (curr_sum == target) return target;
+                if (abs(curr_sum - target) < abs(res - target)) res = curr_sum;
+                if (curr_sum < target) left++;
+                else if (curr_sum > target) right--;
+            }
+        }
+        return res;
+    }
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
