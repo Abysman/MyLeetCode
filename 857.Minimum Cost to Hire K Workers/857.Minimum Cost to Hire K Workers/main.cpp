ba858a4d2b76c9c1e6b1355061a8bda06a13bc05
steven xu <stevenxu169@hotmail.com>
2019-09-14 21:20:38 -0700

    857. Minimum Cost to Hire K Workers

---
+++ 857.Minimum Cost to Hire K Workers/857.Minimum Cost to Hire K Workers/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  857.Minimum Cost to Hire K Workers
+//
+//  Created by stevenxu on 9/14/19.
+//  Copyright © 2019 stevenxu. All rights reserved.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int> q, vector<int> w, int K) {
+        vector<vector<double>> workers;
+        for (int i = 0; i < q.size(); ++i)
+            workers.push_back({(double)(w[i]) / q[i], (double)q[i]});
+        sort(workers.begin(), workers.end());
+        double res = DBL_MAX, qsum = 0;
+        priority_queue<int> pq;
+        for (auto worker: workers) {
+            qsum += worker[1], pq.push(worker[1]);
+            if (pq.size() > K) qsum -= pq.top(), pq.pop();
+            if (pq.size() == K) res = min(res, qsum * worker[0]);
+        }
+        return res;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
