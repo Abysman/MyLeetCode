254182a043bc5f6497dde720540767967919bff6
steven xu <stevenxu169@hotmail.com>
2019-09-19 14:27:49 -0700

    33.Search in Rotated Sorted Array/

--- 33.Search in Rotated Sorted Array/33.Search in Rotated Sorted Array/main.cpp
+++ 33.Search in Rotated Sorted Array/33.Search in Rotated Sorted Array/main.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+//  note that it is possible that mid == low
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -18,15 +19,17 @@ public:
         int low = 0, high = n - 1, curr = 0;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if ((nums[mid] >= nums[0] && target >= nums[0]) ||
-                (nums[mid] < nums[0] && target < nums[0])) {
-                curr = nums[mid];
-                if (curr == target) return mid;
-                else if (curr > target) high = mid - 1;
+            if (nums[mid] == target) return mid;
+            //  mid is in the left half
+            //  since mid is likely to equal low so we should use <=
+            //  or the condition could be changed to nums[mid] < nums[high]
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) high = mid - 1;
                 else low = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
             }
-            else if (target < nums[0]) low = mid + 1;
-            else if (target >= nums[0]) high = mid - 1;
         }
         return -1;
     }
