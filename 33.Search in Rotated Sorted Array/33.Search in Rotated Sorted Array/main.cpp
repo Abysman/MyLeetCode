1339be46026017ee9ff942e0ccb99515074ecfd9
steven xu <stevenxu169@hotmail.com>
2019-09-19 14:03:15 -0700

    33.Search in Rotated Sorted Array/

--- 33.Search in Rotated Sorted Array/33.Search in Rotated Sorted Array/main.cpp
+++ 33.Search in Rotated Sorted Array/33.Search in Rotated Sorted Array/main.cpp
@@ -15,6 +15,27 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
+        int low = 0, high = n - 1, curr = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((nums[mid] >= nums[0] && target >= nums[0]) ||
+                (nums[mid] < nums[0] && target < nums[0])) {
+                curr = nums[mid];
+                if (curr == target) return mid;
+                else if (curr > target) high = mid - 1;
+                else low = mid + 1;
+            }
+            else if (target < nums[0]) low = mid + 1;
+            else if (target >= nums[0]) high = mid - 1;
+        }
+        return -1;
+    }
+};
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
         int low = 0, high = n - 1, mid = -1, curr;
         while (low <= high) {
             mid = low + (high - low) / 2;
