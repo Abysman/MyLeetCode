7b96054e3a5645669107815edc4cbcdf56d6754e
steven xu <stevenxu169@hotmail.com>
2019-09-23 20:51:07 -0700

    855.Exam Room/

--- 855.Exam Room/855.Exam Room/main.cpp
+++ 855.Exam Room/855.Exam Room/main.cpp
@@ -9,6 +9,57 @@
 #include <iostream>
 
 class ExamRoom {
+    vector<int> rooms;
+    int n;
+public:
+    ExamRoom(int N) {
+        n = N;
+    }
+    
+    int seat() {
+        if (rooms.empty()) {
+            rooms.push_back(0);
+            return 0;
+        }
+        
+        int maxDis = max(rooms[0], n - 1 - rooms.back());
+        int pos = -1, index = -1;
+        
+        for (int i = 0; i < rooms.size() - 1; ++i) {
+            maxDis = max((rooms[i + 1] - rooms[i]) / 2, maxDis);
+        }
+        
+        if (rooms[0] == maxDis) {
+            rooms.insert(rooms.begin(), 0);
+            return 0;
+        }
+        
+        for (int i = 0; i < rooms.size() - 1; ++i) {
+            if ((rooms[i + 1] - rooms[i]) / 2 == maxDis) {
+                rooms.insert(rooms.begin() + i + 1, (rooms[i + 1] + rooms[i]) / 2);
+                return rooms[i + 1];
+            }
+        }
+        
+        rooms.push_back(n - 1);
+        return n - 1;
+    }
+    
+    void leave(int p) {
+        for (int i = 0; i < rooms.size(); ++i) {
+            if (rooms[i] == p) rooms.erase(rooms.begin() + i);
+        }
+    }
+};
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * ExamRoom* obj = new ExamRoom(N);
+ * int param_1 = obj->seat();
+ * obj->leave(p);
+ */
+
+class ExamRoom {
 public:
     set<int> seats;
     int n;
