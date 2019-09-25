25f3a13efb41589ddbb1ae479123e8559844fec2
steven xu <stevenxu169@hotmail.com>
2019-09-24 21:12:54 -0700

    378.Kth Smallest Element in a Sorted Matrix/

--- 378.Kth Smallest Element in a Sorted Matrix/378.Kth Smallest Element in a Sorted Matrix/main.cpp
+++ 378.Kth Smallest Element in a Sorted Matrix/378.Kth Smallest Element in a Sorted Matrix/main.cpp
@@ -11,6 +11,8 @@
 
 using namespace std;;
 
+
+//  my binary search
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
@@ -19,32 +21,53 @@ public:
         int l = matrix[0][0], r = matrix[n - 1][n - 1];
         while (l < r) {
             int mid = l + (r - l) / 2;
-            int count = 0, j = n - 1;
-            for (int i = 0; i < n; ++i) {
-                while (j >= 0 && matrix[i][j] > mid) {
-                    j--;
+            int count = 0;
+            int row = 0, col = n - 1;
+            while (row < n && col >= 0) {
+                if (matrix[row][col] <= mid) {
+                    row++;
+                    count += col + 1;
+                } else {
+                    col--;
                 }
-                count += j + 1;
             }
-            // if (count == k) return mid;
-            // else if (count > k) r = mid + 1;
-            // else l = mid + 1;
-            if(count<k)
-                l=mid+1;
-            else if(count == k) return mid;
-            else
-                r=mid;
+            if (count < k) l = mid + 1;
+            else r = mid;
         }
         return l;
     }
 };
+
+//class Solution {
+//public:
+//    int kthSmallest(vector<vector<int>>& matrix, int k) {
+//        if (matrix.empty() || matrix[0].empty()) return -1;
+//        int n = matrix.size();
+//        int l = matrix[0][0], r = matrix[n - 1][n - 1];
+//        while (l < r) {
+//            int mid = l + (r - l) / 2;
+//            int count = 0, j = n - 1;
+//            for (int i = 0; i < n; ++i) {
+//                while (j >= 0 && matrix[i][j] > mid) {
+//                    j--;
+//                }
+//                count += j + 1;
+//            }
+//            if(count<k)
+//                l=mid+1;
+//            else
+//                r=mid;
+//        }
+//        return l;
+//    }
+//};
 
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     vector<vector<int>> t = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
     Solution s = Solution();
-    s.kthSmallest(t, 8);
+    cout << s.kthSmallest(t, 8) << "\n";
     
     std::cout << "Hello, World!\n";
     return 0;
