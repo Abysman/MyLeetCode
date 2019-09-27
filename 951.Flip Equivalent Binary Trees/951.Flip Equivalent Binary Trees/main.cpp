890bfe50f273524eb71a4648ac91c07232c8e595
steven xu <stevenxu169@hotmail.com>
2019-09-27 12:35:06 -0700

    951.Flip Equivalent Binary Trees/

---
+++ 951.Flip Equivalent Binary Trees/951.Flip Equivalent Binary Trees/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  951.Flip Equivalent Binary Trees
+//
+//  Created by stevenxu on 9/27/19.
+//  Copyright © 2019 stevenxu. All rights reserved.
+//
+
+#include <iostream>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+        if (root1->val == root2->val) {
+            return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) ||
+                (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+        } else return false;
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
