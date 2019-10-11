1c175883fd479222a93262b2f11478a34fd5cd5c
steven xu <stevenxu169@hotmail.com>
2019-10-11 14:15:20 -0700

    72.Edit Distance/

--- 72.Edit Distance/72.Edit Distance/main.cpp
+++ 72.Edit Distance/72.Edit Distance/main.cpp
@@ -11,24 +11,18 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int word1_length = word1.size(), word2_length = word2.size();
-        vector<vector<int>> record(word1_length + 1, vector<int> (word2_length + 1, 0));
-        for (int i = 1; i <= word1_length; ++i) {
-            record[i][0] = i;
-        }
-        for (int j = 1; j <= word2_length; ++j) {
-            record[0][j] = j;
-        }
-        for (int i = 1; i <= word1_length; ++i) {
-            for (int j = 1; j <= word2_length; ++j) {
-                if (word1[i - 1] == word2[j - 1]) {
-                    record[i][j] = record[i - 1][j - 1];
-                } else {
-                    record[i][j] = min(record[i - 1][j - 1], min(record[i - 1][j], record[i][j - 1])) + 1;
-                }
+        int n1 = word1.size(), n2 = word2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int i = 1; i <= n1; ++i) dp[i][0] = i;
+        for (int i = 1; i <= n2; ++i) dp[0][i] = i;
+        for (int i = 1; i <= n1; ++i) {
+            for (int j = 1; j <= n2; ++j) {
+                if (word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = dp[i - 1][j - 1] + 1;
+                dp[i][j] = min(dp[i][j], min(dp[i - 1][j], dp[i][j - 1]) + 1);
             }
         }
-        return record[word1_length][word2_length];
+        return dp[n1][n2];
     }
 };
 
