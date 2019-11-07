7949fba0f67941cd0081314b251ac375f9f2ddcf
steven xu <stevenxu169@hotmail.com>
2019-11-06 16:47:30 -0800

    704.Binary Search/

---
+++ 704.Binary Search/704.Binary Search/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  704.Binary Search
+//
+//  Created by stevenxu on 11/6/19.
+//  Copyright © 2019 stevenxu. All rights reserved.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
