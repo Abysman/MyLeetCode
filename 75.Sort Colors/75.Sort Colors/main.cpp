99f04e8dac8385be5be34015749ad16debc159a8
steven xu <stevenxu169@hotmail.com>
2019-11-14 15:52:04 -0800

    75.Sort Colors/

--- 75.Sort Colors/75.Sort Colors/main.cpp
+++ 75.Sort Colors/75.Sort Colors/main.cpp
@@ -22,24 +22,24 @@ using namespace std;
 //    }
 //};
 
-//class Solution {
-//public:
-//    void sortColors(vector<int>& nums) {
-//        int r = -1, w = -1, b = -1;
-//        for (int i = 0; i < nums.size(); ++i) {
-//            if (nums[i] == 0) {
-//                nums[++b] = 2;
-//                nums[++w] = 1;
-//                nums[++r] = 0;
-//            }
-//            else if (nums[i] == 1) {
-//                nums[++b] = 2;
-//                nums[++w] = 1;
-//            }
-//            else if (nums[i] == 2) nums[++b] = 2;
-//        }
-//    }
-//};
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, mid = 0, right = n - 1;
+        while (left <= mid && mid <= right) {
+            if (nums[mid] == 0) {
+                swap(nums[mid], nums[left]);
+                left++;
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums[mid], nums[right]);
+                right--;
+            } else mid++;
+            
+        }
+    }
+};
 
 class Solution {
 public:
