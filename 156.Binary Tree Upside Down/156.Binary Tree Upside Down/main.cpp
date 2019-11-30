5f54a519eff29aa6ae332b2c0f6e24ce8288afcd
steven xu <stevenxu169@hotmail.com>
2019-11-30 14:57:45 -0800

    156.Binary Tree Upside Down/

--- 156.Binary Tree Upside Down/156.Binary Tree Upside Down/main.cpp
+++ 156.Binary Tree Upside Down/156.Binary Tree Upside Down/main.cpp
@@ -2,57 +2,35 @@
 //  main.cpp
 //  156.Binary Tree Upside Down
 //
-//  Created by Abysman on 2018/4/20.
-//  Copyright © 2018年 Abysman. All rights reserved.
+//  Created by stevenxu on 11/30/19.
+//  Copyright © 2019 stevenxu. All rights reserved.
 //
 
 #include <iostream>
-#include <vector>
-
-/*
- *  分治
- *  二分法
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
  */
-
-using namespace std;
-
-struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
-
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if (nums.empty()) {
-            return NULL;
-        }
-        return toBST(nums, 0, nums.size() - 1);
-    }
-    
-    TreeNode* toBST(vector<int>&nums, int start, int end) {
-        if (start == end) {
-            return new TreeNode(nums[start]);
-        }
-        if (start > end) {
-            return NULL;
-        }
-        int mid = (start + end) / 2;
-        TreeNode* root = new TreeNode(nums[mid]);
-        
-        root->left = toBST(nums, start, mid - 1);
-        root->right = toBST(nums, mid + 1, end);
-        
-        return root;
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if (root == nullptr || root->left == nullptr) return root;
+        TreeNode *left = root->left, *right = root->right;
+        TreeNode *new_root = upsideDownBinaryTree(root->left);
+        left->right = root;
+        left->left = right;
+        root->left = nullptr;
+        root->right = nullptr;
+        return new_root;
     }
 };
-
 int main(int argc, const char * argv[]) {
     // insert code here...
-    Solution sol = Solution();
-    vector<int> nums = {-10,-3,0,5,9};
-    sol.sortedArrayToBST(nums);
+    std::cout << "Hello, World!\n";
     return 0;
 }
